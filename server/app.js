becfc3fdd97947269d005b74ffc3bcde2e09cb05
varsha-reddy2 <ugs21144_it.varsha@cbit.org.in>
2023-09-29 11:22:51 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser=require('body-parser')
+const mongoose = require('mongoose')
+const cors=require('cors')
+const url = 'mongodb+srv://varsha:varsha@cluster0.h4iogta.mongodb.net/it3fsd?retryWrites=true&w=majority&appName=AtlasApp'
+const app = express()
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+con.on('open', () =>
+{
+console.log('connected...')
+})
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
+app.listen(9000, () =>
+{
+console.log('Server started')
+})
+
